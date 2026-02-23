24429cce7615f76c52a1aa4459e046e5fd34f3f7
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-23 15:01:24 -0500

    nesting routes example

--- src/main.js
+++ src/main.js
@@ -11,9 +11,22 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/teams" },
-    { path: "/teams", component: TeamsList /* alias: '/' */ },
+    {
+      // alias: "/",
+      name: "teams",
+      path: "/teams",
+      component: TeamsList,
+      children: [
+        {
+          name: "team-members",
+          path: "/teams/:teamId",
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    // { name: "team-members", path: "/teams/:teamId", component: TeamMembers, props: true },
     { path: "/users", component: UsersList },
-    { path: "/teams/:teamId", component: TeamMembers, props: true },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
