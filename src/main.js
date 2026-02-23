3dd8412038dfd33a129911a2b33d7c5b5920fa5b
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-23 16:00:44 -0500

    reorganize files

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./App.vue";
-import TeamsList from "./components/teams/TeamsList.vue";
-import UsersList from "./components/users/UsersList.vue";
+import TeamsList from "./pages/TeamsList.vue";
+import UsersList from "./pages/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
-import NotFound from "./components/nav/NotFound.vue";
+import NotFound from "./pages/NotFound.vue";
 import TeamsFooter from "./components/teams/TeamsFooter.vue";
 import UsersFooter from "./components/users/UsersFooter.vue";
 
--- src/pages/UsersList.vue
+++ src/pages/UsersList.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import UserItem from "./UserItem.vue";
+import UserItem from "@/components/users/UserItem.vue";
 
 export default {
   components: {
